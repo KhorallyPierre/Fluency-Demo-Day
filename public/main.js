d706ad788e46aecb7a3973841f94cb2fcad792a7
Khorally Pierre <kvpierre@gmail.com>
2021-04-24 22:29:57 -0400

    timer js implementation

--- public/main.js
+++ public/main.js
@@ -1,80 +1,24 @@
-// var trash = document.getElementsByClassName("fa-trash");
 var chat = document.getElementsByClassName('connectToChat');
 
+document.addEventListener('DOMContendLoaded', () => {
+  const timeLeftDisplay = document.querySelector('#time-left')
+  const startTime = document.querySelector('.timer2') // button that should trigger the start of the timer
+  timeLeft = 1500
 
+  function countDown() {
+    setInterval(function() {
+      if (timeLeft <= 0) {
+        clearInterval(timeLeft = 0)
+      }
 
-//
-// Array.from(thumbUp).forEach(function(element) {
-//   element.addEventListener('click', function() {
-//     const name = this.parentNode.parentNode.childNodes[1].innerText
-//     const msg = this.parentNode.parentNode.childNodes[3].innerText
-//     const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-//     fetch('messages', {
-//         method: 'put',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//           'name': name,
-//           'msg': msg,
-//           'thumbUp': thumbUp
-//         })
-//       })
-//       .then(response => {
-//         if (response.ok) return response.json()
-//       })
-//       .then(data => {
-//         console.log(data)
-//         window.location.reload(true)
-//       })
-//   });
-// });
-// Array.from(thumbDown).forEach(function(element) {
-//   element.addEventListener('click', function() {
-//     const name = this.parentNode.parentNode.childNodes[1].innerText
-//     const msg = this.parentNode.parentNode.childNodes[3].innerText
-//     const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-//     fetch('messages/down', {
-//         method: 'put',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//           'name': name,
-//           'msg': msg,
-//           'thumbUp': thumbUp
-//         })
-//       })
-//       .then(response => {
-//         if (response.ok) return response.json()
-//       })
-//       .then(data => {
-//         console.log(data)
-//         window.location.reload(true)
-//       })
-//   });
-// });
-// deleted langauges on routes instead of here
-// Array.from(trash).forEach(function(element) {
-//
-//   element.addEventListener('click', function() {
-//     console.log('event is working')
-//     const language = this.parentNode.parentNode.getElementById('nameOfLang').innerText
-//     fetch('profile', {
-//       method: 'delete',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         'language': language
-//
-//       })
-//     }).then(function(response) {
-//       window.location.reload()
-//     })
-//   });
-// });
+      timeLeftDisplay.innerText = "Time Left: " + timeLeft
+      timeLeft -= 1
+    }, 1000)
+  }
+startTime.addEventListener('click', countDown)
+window.addEventListener('load', countDown)
 
+})
 
 
 
@@ -84,7 +28,7 @@ Array.from(chat).forEach(function(element) {
     console.log('you clicked to chat', element.id)
     // call fetch to that pair API
     // pair API should match you with someone to talk to
-    const url= '/pair?' + 'learning=' + element.id
+    const url = '/pair?' + 'learning=' + element.id
     fetch(url)
       .then(response => response.json())
       .then(data => {
@@ -92,22 +36,9 @@ Array.from(chat).forEach(function(element) {
         window.location = '/profile'
 
 
-        console.log(data)});
+        console.log(data)
+      });
 
 
   })
 });
-
-// let chatForm = document.querySelectorAll('.searchChat');
-// chatForm.addEventListener('submit', searchChat);
-// function searchChat(e) {
-//   e.preventDefault();
-//   const learning = e.target.childNodes[1].getAttribute('id');
-//   console.log('GOODBYE')
-//   console.log(e.target.childNodes[1].getAttribute('id'));
-//   fetch('/pair', {
-//     method: 'post',
-//     headers: {'Content-Type' : 'application/json'},
-//     body: JSON.stringify({ learning: learning })
-//   }).then( () => {window.location.reload(); })
-// }
--- public/videoScript.js
+++ public/videoScript.js
@@ -48,7 +48,7 @@ function connectToNewUser(userId, stream) {
   call.on('close', () => {
     video.remove()
   })
-// peer is being decleared (not a good practice)
+  // peer is being decleared (not a good practice)
   peers[userId] = call
 }
 // event listenever for when video stream begins
@@ -60,55 +60,19 @@ function addVideoStream(video, stream) {
   videoGrid.append(video)
 }
 
-
-// student will be in room alone after making request
-
-// will provide ID to peer (no new one is needed)
-// teacher will interract with peer and provide its id
-// teacher will need its id to identify themsevles and student to complete request to peer
-// teacher will initiate call using peer
-// peer js library will make introduction (peerJS is not the same as peer)
-
-// 2021-04-16
-// 'use strict';
-// function readStudentIdFromURL() {
-//   var queryString = location.search;
-//   var urlSearchParameters = new URLSearchParams(queryString);
-//   return urlSearchParameters.get('student_id');
+// document.querySelector('timer2')
+// const startingMinutes = 15;
+// let time = startingMinutes * 60;
+//
+// const countdownEl = getElementById('countdown');
+//
+// setInterval(updateCountdown, 1000);
+//
+// function updateCountdown() {
+//   const minutes = Math.floor(time / 60);
+//   let seconds = time % 60;
+//
+//   countdownEl.innerHTML = `${minutes}: ${seconds}`;
+//
+//   time--;
 // }
-// // Student code:
-// {
-//   var studentId = readStudentIdFromURL();
-//   const peer = new Peer(studentId);
-//   // wait for call...
-// }
-/**
- * we'll need to do the same thing for the teacher, except remember that the
- * teacher needs *two* pieces of information. They need their own ID *and* the
- * ID of the student with whom they're about to start a call
- */
-// Teacher code:
-// {
-//   // looks similar to start
-//   var teacherId = readTeacherIdFromURL();
-//   const peer = new Peer(teacherId);
-//   //but then:
-//   /**
-//    * (code to get mediaStream goes here)
-//    */
-//   // initiate call
-//   var studentId = readStudentIdFromURL();
-//   var call = peer.call(studentId, mediaStream);
-// }
-/**
- * What we're calling the "student code" and the "teacher code" can go in the
- * same file. You just need to recognize which one you should use. One way you
- * could decide which one to use is to see if the teacher ID is present.
- *
- * Remember: we only need to give the teacher ID to the teacher. So if this
- * script ran for both the student and for the the teacher, then you could
- * decide which of the two examples above to run by seeing if the teacher ID
- * was present.
- */
-// Don't forget to study the PeerJS documentation!
-// https://peerjs.com/docs.html#peer
